7be2cdd42bf747f1474bbb3117f243a833cb76ac
Alisa <91307985+bouffee@users.noreply.github.com>
2023-08-04 02:36:49 +0300

    Update Example2.py
    Added few basic operations for flowFile

--- EducationalScripts/Example2.py
+++ EducationalScripts/Example2.py
@@ -1,5 +1,4 @@
 import sys
-from tempfile import TemporaryFile
 from org.apache.commons.io import IOUtils
 from java.nio.charset import StandardCharsets
 from org.apache.nifi.processor.io import StreamCallback
@@ -7,22 +6,31 @@ from org.apache.nifi.processor.io import StreamCallback
 reload(sys)
 sys.setdefaultencoding('UTF-8')
 additionalContent = "test"
+flowFileAttr = dict()
+newAttrs = { "newAttr1": "apple",
+           "newAttr2": str(12)}
 
 class ModifyContent(StreamCallback):
     def __init__(self):
         pass
 
     def process(self, inputStream, outputStream):
-        global additionalContent
+        global flowFileAttr
         flowFileContent = IOUtils.toString(inputStream, StandardCharsets.UTF_8)
-        flowFileContent = flowFileContent + "\n" + additionalContent
+        for key, value in flowFileAttr.items():
+            flowFileContent = flowFileContent + "\n" + str(key) + ": " + str(value)
         outputStream.write(bytearray(flowFileContent.encode('utf-8')))       
 
 flowFile = session.get()
 
 if flowFile is not None:
     try:
+        flowFileAttr = flowFile.getAttributes()
         session.write(flowFile, ModifyContent())
+        log.info("Content has been modified!")
+        flowFile = session.putAttribute(flowFile, "modify", str(True))
+        flowFile = session.putAllAttributes(flowFile, newAttrs)
+
     except Exception as e:
         flowFile = session.putAttribute(flowFile, "error", str(e))
         session.transfer(flowFile, REL_FAILURE)
