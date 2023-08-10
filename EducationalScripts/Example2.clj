e5189f7267e7e85175a86c24ef3e696f1b941c33
Alisa <91307985+bouffee@users.noreply.github.com>
2023-08-10 12:52:30 +0300

    Update Example2.clj
    Remove all unnecessary functions. Added putAllAttributes method realization

--- EducationalScripts/Example2.clj
+++ EducationalScripts/Example2.clj
@@ -3,32 +3,22 @@
 (import 'org.apache.nifi.processor.io.StreamCallback)
 
 ;; (def additionalContent "test")
-;; (def newAttrs {"newAttr1" "apple"
-;;                "newAttr2" (str 12)})
-
-(defn put-attribute [flow]
-  (.putAttribute session flow "modify" "true"))
-
-;; (defn send-log []
-;;   (.info log "Content has been modified!"))
-
+(def newAttrs {"newAttr1" "apple"
+               "newAttr2" (str 12)})
 
 (defn stream-callback [flow content]
   (reify StreamCallback
     (process [this inputStream outputStream]
       (.write outputStream
-              (.getBytes (str (slurp inputStream) "\n" (apply str content )))))))
+              (.getBytes (str (slurp inputStream) "\n" content))))))
 
 (defn write-content[flow content]
   (.write session flow (stream-callback flow content)))
 
-(defn success-transfer [flow]
-  (.transfer session flow REL_SUCCESS))
-
 (let [flowFile (.get session)]
   (def flowFileAttrs (.getAttributes flowFile))
   (write-content flowFile flowFileAttrs)
-  (-> flowFile
-      put-attribute
-      success-transfer)
-  (.info log "Content has been modified!"))
+  (.putAllAttributes session flowFile newAttrs)
+  (.putAttribute session flowFile "modify" "true") 
+  (.info log "Content has been modified!")
+  (.transfer session flowFile REL_SUCCESS))
